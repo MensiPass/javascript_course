15e5d2bc950fef091fc4ecf9e564572768af61a7
Mensura Pasic <mensipass@gmail.com>
2024-01-23 12:44:44 +0100

    Operators exercise files

---
+++ operators/script.js
@@ -0,0 +1,57 @@
+// Arithmetic operators
+
+let x
+
+x=5+5
+x=5-5
+x=5*5
+x=5/5
+x=7%5
+
+//concatenation
+
+x='Hello'+ ' '+ 'World'
+
+//exponent
+
+x=2**3
+
+//increment
+x=1
+
+//x=x+1
+x++
+//decrement
+//x=x-1
+x--
+//Assignmet operators
+x=10
+x+=5
+x-=5
+x*=5
+x/=5
+x%=5
+x**=5
+
+//Comparison operators
+//Equal to (just the value, not the type)
+x=2=='2'
+
+//equal to (type and value)
+x=2==='2'
+
+//not equal to (just the value not type)
+x=2!='2'
+
+//not equal to(type and value)
+x=2!==2
+
+//greather than and less than
+
+x=10>5
+x=10<5
+x=10<=5
+x010>=5
+
+
+console.log(x)
